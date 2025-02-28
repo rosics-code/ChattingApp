6f43dc1ff1f51062cd7cda0e34309a3352891162
rosics-code <roblox888i12345@gmail.com>
2025-02-28 15:12:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Allow frontend to connect
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change this to your frontend URL in production
+    allow_origins=["https://chattingapp-ssfq.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
